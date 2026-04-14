c3a49642c79c93918fbaa891e88a9a5ee588358c
maxtico <max.tico@alumni.esci.upf.edu>
2026-04-14 12:19:46 +0200

    polars bio unary ops

---
+++ scripts/unary/polars_bio/cluster.py
@@ -0,0 +1,14 @@
+import polars_bio as pb
+
+from lib.helpers import get_file, write_result
+
+df = get_file("polars_bio")
+print(df)
+
+df = pb.cluster(
+    df,
+    cols=["chrom", "start", "end"],
+    output_type="pandas.DataFrame",
+)
+
+write_result("unary", str(len(df.collect())))
